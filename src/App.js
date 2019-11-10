4d5934d297d07f91301523b78c13a923b743bfd3
Waylon Dixon <wayjay4@yahoo.com>
2019-11-10 11:36:47 -0700

    accessing state values, implemented useSelector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import {useSelector} from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <p>Counter is currently: {counter}</p>
+      <p>{isLogged ? "you are logged in" : "please login"}</p>
     </div>
   );
 }
